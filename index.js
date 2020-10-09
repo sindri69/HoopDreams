a5c45518349ef0b5657e10a9c330efb1261a097b
sindri69 <sindrii17@ru.is>
2020-10-09 13:33:15 +0000

    top index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
 const server = new ApolloServer({
-    /*
-        Add typeDefs
-        Add resolvers
-    */
+
+    typeDefs,
+    resolvers
 });
 
 server.listen()
