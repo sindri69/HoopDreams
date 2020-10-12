9d003229f7a239f86d5dc4370a1dfca1b25aef9e
sindri69 <sindrii17@ru.is>
2020-10-12 14:02:41 +0000

    index

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema'); //setup later (schema) 
-const resolvers = {}; //setup later (resolvers)
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const basketballfieldService = require('./services');
+const myDB = require('./data/db');
+
 
 const server = new ApolloServer({
+
     typeDefs,
-    resolvers
+    resolvers,
+    context: {
+        db: myDB,
+        services: basketballfieldService
+    }
+    
 });
 
 server.listen()
