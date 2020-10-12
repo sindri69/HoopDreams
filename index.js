9896a05497b4c71dfaf6967f3666dc75f14b657a
Róslín <roslin19@ru.is>
2020-10-12 14:03:30 +0000

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema'); //setup later (schema) 
 const resolvers = {}; //setup later (resolvers)
+const myDatabase = require('./data/db')
 
 const server = new ApolloServer({
     typeDefs,
