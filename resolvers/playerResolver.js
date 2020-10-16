7ddfd57470445de460fde99eb2ae6513433c38d9
Róslín <roslin19@ru.is>
2020-10-16 11:45:28 +0000

    player is ready

--- resolvers/playerResolver.js
+++ resolvers/playerResolver.js
@@ -1,4 +1,5 @@
 const { NotFoundError } = require("../errors")
+const ObjectId = require ("mongoose").Types.ObjectId
 
 module.exports = {
     queries: {
@@ -6,20 +7,19 @@ module.exports = {
             const myDB = context.db;
             const players = await myDB.Player.find({available: true})
             return players
-<<<<<<< HEAD
         }, 
-=======
-        },
->>>>>>> 2d4ff3909f08321702ee2d7da61dad67784dbc42
         player: async(parent, args, context) => {
             const myDB = context.db;
             const {id} = args
-            const player = await myDB.Player.find({_id: id, available: true}, function(err, result) {
-                if(err) {return null}
-                console.log(result)
-                return result
-            });
-            if (player == null) {throw new NotFoundError('This player was not available or something')}
+
+            if (!ObjectId.isValid(id)) throw new NotFoundError('This id is not valid')
+
+            const player = await myDB.Player.findOne({_id: id, available: true},function(err,result){
+                if (err) {console.log(err); return null}
+                else return result;
+            })
+            console.log(player)
+            if (player == null) {throw new NotFoundError('This player does not exist')}
             return player
         }},
     mutations: {
@@ -36,7 +36,8 @@ module.exports = {
         removePlayer: async (parent, args, context) => {
             const { id } = args;
             const myDB  = context.db;
-            const result = await myDB.Player.findById({_id: id});
+            if (!ObjectId.isValid(id)) throw new NotFoundError('This id is not valid')
+            const result = await myDB.Player.findOne({_id: id, available:true});
             if(result == null) {throw new NotFoundError('This player does not exist');}
             result.available = false
             result.save()
@@ -44,10 +45,17 @@ module.exports = {
         },
 
         updatePlayer: async(parent, args, context) => {
-            const {id, pickupgame, name} = args;
+            //instructions unclear dick stuck in a blender
+            //Only updates name because the functions remove player from a pickupGame and 
+            //add player to a pickupgame update the playerGames list automatically
+            const {id, name} = args;
+            if (!ObjectId.isValid(id)) throw new NotFoundError('This id is not valid')
             const myDB = context.db;
-            const result = await myDB.Players.find({_id: id, available: true})
-            //Hvað erum við að uppfæra
+            const result = await myDB.Player.findOneAndUpdate({_id: id, available: true}, {name: name}, {
+                new: true
+            })
+            if (result == null) throw new NotFoundError('This player cannot be updated since he does not exist')
+            return result
         }
 
     }
